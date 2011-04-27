7d6bb4e22e417420870974400e23fb2e81659c17
Hugo Duncan <hugo@hugoduncan.org>
2011-04-26 23:40:12 -0400

    Remove clj-stomp dependency

--- project.clj
+++ project.clj
@@ -4,7 +4,6 @@
   :resources-path "src/main/resources"
   :test-path "src/test/clojure"
   :aot [swank-clj.main]
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [clj-stomp "0.0.1-SNAPSHOT"]]
+  :dependencies [[org.clojure/clojure "1.2.0"]]
   :dev-dependencies [[swank-clojure "1.2.1"]
                      [lein-swank-clj "1.0.0-SNAPSHOT"]])
