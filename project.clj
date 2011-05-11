51b3f5d2def7f79bd7b410771016ea13ea39885d
Hugo Duncan <hugo@hugoduncan.org>
2011-05-11 10:43:40 -0400

    Updated project.clj, release notes and readme for 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject swank-clj "0.1.0-SNAPSHOT"
+(defproject swank-clj "0.1.0"
   :description "Another swank for clojure"
   :source-path "src/main/clojure"
   :resources-path "src/main/resources"
