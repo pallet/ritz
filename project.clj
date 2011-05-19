64f39926a863b49a5b73bb2331d6da6efa26e38b
Hugo Duncan <hugo@hugoduncan.org>
2011-05-18 23:23:18 -0400

    disable AOT
    In order for swank-clj to work across clojure versions, remove the aot.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :source-path "src/main/clojure"
   :resources-path "src/main/resources"
   :test-path "src/test/clojure"
-  :aot [swank-clj.main]
+  ;; :aot [swank-clj.main]
   :dependencies [[org.clojure/clojure "1.2.0"]]
   :dev-dependencies [[swank-clojure "1.2.1"]
                      [lein-swank-clj "1.0.0-SNAPSHOT"]
