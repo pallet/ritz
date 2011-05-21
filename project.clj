c652f89f02bfe68fc557198d0c40d9035fdc6a87
Hugo Duncan <hugo@hugoduncan.org>
2011-05-20 22:51:32 -0400

    Update version for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject swank-clj "0.1.6"
+(defproject swank-clj "0.1.7-SNAPSHOT"
   :description "Another swank for clojure"
   :source-path "src/main/clojure"
   :resources-path "src/main/resources"
