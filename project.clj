e331b223ad45c565ec120162699b587839f955d8
Hugo Duncan <hugo@hugoduncan.org>
2011-09-29 17:08:28 -0400

    Put all clojure source dependencies into lein-multi config

--- project.clj
+++ project.clj
@@ -5,8 +5,7 @@
   :test-path "src/test/clojure"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [useful "0.4.0"]]
-  :dev-dependencies [[org.clojure/clojure "1.2.1" :classifier "sources"]
-                     [lein-multi "1.0.0"]
+  :dev-dependencies [[lein-multi "1.0.0"]
                      [org.cloudhoist/pallet "0.6.3-SNAPSHOT"]
                      [org.cloudhoist/stevedore "0.6.1-SNAPSHOT"]
                      [org.cloudhoist/automated-admin-user "0.5.1-SNAPSHOT"]
@@ -19,12 +18,10 @@
                      [vmfest "0.2.3"]]
   :multi-deps {"1.2.0" [[org.clojure/clojure "1.2.0"]
                         [clojure-source "1.2.0"]]
-               "1.3" [[org.clojure/clojure "1.3.0-master-SNAPSHOT"]
-                      [org.clojure/clojure "1.3.0-master-SNAPSHOT"
-                       :classifier "sources"]]
-               "1.3.0-alpha8" [[org.clojure/clojure "1.3.0-alpha8"]
-                      [org.clojure/clojure "1.3.0-alpha8"
-                       :classifier "sources"]]}
+               "1.2.1" [[org.clojure/clojure "1.2.1"]
+                        [org.clojure/clojure "1.2.1" :classifier "sources"]]
+               "1.3" [[org.clojure/clojure "1.3.0"]
+                      [org.clojure/clojure "1.3.0" :classifier "sources"]]}
   :local-repo-classpath true
   :repositories
   {"sonatype-snapshots" "https://oss.sonatype.org/content/repositories/snapshots"
