75c967685888fd3e253e16193e9ca0600daee438
Hugo Duncan <hugo@hugoduncan.org>
2011-10-31 23:12:10 -0400

    Update version to 0.2.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ritz "0.2.0"
+(defproject ritz "0.2.1-SNAPSHOT"
   :description "Another swank server for clojure in SLIME"
   :source-path "src/main/clojure"
   :resources-path "src/main/resources"
