ff83e26cecc8c64195e5e7dc557e21575489b139
Hugo Duncan <hugo@hugoduncan.org>
2012-09-06 18:03:46 -0400

    Fix versions in top level project.clj file

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject ritz "0.4.0"
+(defproject ritz "0.4.1-SNAPSHOT"
   :description "Another swank server for clojure in SLIME"
   :url "https://github.com/pallet/ritz"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :scm {:url "git@github.com:pallet/ritz.git"}
   :sub ["repl-utils" "debugger" "nrepl" "swank" "lein-ritz"]
-  :plugins [[lein-sub "0.2.2-SNAPSHOT"]]
+  :plugins [[lein-sub "0.2.1"]]
   :aliases {"clean" ["sub" "clean"]
             "install" ["sub" "install"]
             "deploy" ["sub" "deploy"]
