fc8f52d22dc5ce832b96bc5a1643420b2bf5c273
Hugo Duncan <hugo@hugoduncan.org>
2012-09-11 16:05:12 -0400

    Fix top level test alias to include default profiles

--- project.clj
+++ project.clj
@@ -9,6 +9,6 @@
   :aliases {"clean" ["sub" "clean"]
             "install" ["sub" "install"]
             "deploy" ["sub" "deploy"]
-            "test" ["sub" "with-profile" "jdk1.7" "test"]
+            "test" ["sub" "with-profile" "default,jdk1.7" "test"]
             "doc" ["sub" "with-profile" "codox,jdk1.7" "doc"]
             "set-version" ["sub" "with-profile" "release" "set-version"]})
