f0f1e1971999d3d94a5af2f9c5280ad5e07385a8
Hugo Duncan <hugo@hugoduncan.org>
2012-10-04 09:38:57 -0400

    Add nrepl-hornetq profiles.clj

---
+++ nrepl-hornetq/profiles.clj
@@ -0,0 +1,14 @@
+{:codox {:codox {:writer codox-md.writer/write-docs
+                 :output-dir "../doc/0.4/nrepl-hornetq/api"}
+         :dependencies [[codox-md "0.1.0"]]
+         :pedantic :warn}
+ :marginalia {:pedantic :warn
+              :dir "../doc/0.4/nrepl-hornetq/source"}
+ :release
+ {:set-version
+  {:updates [{:path "README.md"
+              :no-snapshot true
+              :search-regex #"ritz/ritz-nrepl-hornetq \"\d+\.\d+\.\d+\""}
+             {:path "README.md"
+              :no-snapshot true
+              :search-regex #"lein-ritz \"\d+\.\d+\.\d+\""}]}}}
