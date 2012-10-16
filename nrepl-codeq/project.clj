31864380c4bf1fa84b84d30c8bf5f905e47f5542
Hugo Duncan <hugo@hugoduncan.org>
2012-10-16 10:46:33 -0400

    Fix repl-utils dependency in nrepl-codeq

--- nrepl-codeq/project.clj
+++ nrepl-codeq/project.clj
@@ -7,4 +7,4 @@
                  [org.clojure/tools.nrepl "0.2.0-beta9"]
                  [com.datomic/datomic-free "0.8.3551"]
                  [datomic/codeq "0.1.0-SNAPSHOT"]
-                 [ritz/ritz-nrepl-codeq "0.5.1-SNAPSHOT"]])
+                 [ritz/ritz-repl-utils "0.5.1-SNAPSHOT"]])
