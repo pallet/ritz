8e249a0b21817e998bda46403308e8ea4415e60f
Hugo Duncan <hugo@hugoduncan.org>
2012-10-30 23:38:06 -0400

    Add test code to run transactor and import codeq data
    This is nasty in that it relies on unofficial jars for the datomic transactor
    and for codeq.

--- nrepl-codeq/profiles.clj
+++ nrepl-codeq/profiles.clj
@@ -1,4 +1,8 @@
-{:codox {:codox {:writer codox-md.writer/write-docs
+{:dev {:dependencies [[com.cemerick/pomegranate "0.0.13"
+                       :exclusions [org.slf4j/slf4j-api org.clojure/clojure]]
+                      [classlojure "0.6.6"
+                       :exclusions [org.slf4j/slf4j-api org.clojure/clojure]]]}
+ :codox {:codox {:writer codox-md.writer/write-docs
                  :output-dir "../doc/0.5/nrepl-codeq/api"}
          :dependencies [[codox-md "0.1.0"]]
          :pedantic :warn}
--- nrepl-codeq/test/ritz/nrepl/middleware/codeq_test.clj
+++ nrepl-codeq/test/ritz/nrepl/middleware/codeq_test.clj
@@ -2,6 +2,9 @@
   (:use
    clojure.test
    ritz.nrepl.middleware.codeq
+   [cemerick.pomegranate.aether :only [dependency-files resolve-dependencies]]
+   [classlojure.core :only [classlojure eval-in]]
+   [clojure.java.io :only [file output-stream]]
    [clojure.stacktrace :only [print-cause-trace]]
    [clojure.tools.nrepl.server :only [handle* unknown-op]]
    [clojure.tools.nrepl.transport :only [recv]])
@@ -32,22 +35,102 @@ on the given transport with the handler, until the atom is set to false."
   (or (System/getProperty "ritz.datomic.url")
       "datomic:free://localhost:4334/git"))
 
-(deftest codeq-def-test
-  (let [[in out transport] (queue-transport)
-        handler (-> unknown-op wrap-codeq-def)
-        [handle-f a] (stoppable-handle handler transport)
-        handle-loop (future
-                      (try
-                        (handle-f)
-                        (catch Exception e (print-cause-trace e))))]
-    ;; set the classpath
-    (.offer in {:op "codeq-def" :symbol "ritz.logging/trace"
-                :datomic-url (datomic-url) :id 0})
-    (is (= {:value
-            [["(defmacro trace\n  [fmt-str & args]\n  `(log :trace ~fmt-str ~@args))" "Sat Jul 02 22:08:35 EDT 2011"]]
-            :id 0}
-           (.take out)))
+(defn save-properties
+  [f property-map]
+  (with-open [^java.io.OutputStream out (output-stream f)]
+    (let [props (java.util.Properties.)]
+      (doseq [[k v] property-map]
+        (.setProperty props (name k) (str v)))
+      (.store props out "properties for nrepl-codeq tests"))))
 
-    ;; shut down the handle loop
-    (reset! a false)
-    (.offer in {:op "something to stop the handle loop"})))
+(defn port-reachable?
+  ([ip port timeout]
+     (let [socket (doto (java.net.Socket.)
+                    (.setReuseAddress false)
+                    (.setSoLinger false 1)
+                    (.setSoTimeout timeout))]
+       (try
+         (.connect socket (java.net.InetSocketAddress. ip port))
+         true
+         (catch java.io.IOException _)
+         (finally
+           (try (.close socket) (catch java.io.IOException _))))))
+  ([ip port]
+     (port-reachable? ip port 2000)))
+
+(defn run-transactor
+  []
+  (let [f (java.io.File/createTempFile "nrepl-codeq" ".properties")
+        cl (classlojure
+            (dependency-files
+             (resolve-dependencies
+              :repositories {"clojars" "https://clojars.org/repo/"
+                             "central" "http://repo1.maven.org/maven2/"}
+              :coordinates '[[org.cloudhoist/datomic-free-transactor "0.8.3551"]
+                             [ch.qos.logback/logback-classic "1.0.0"]]
+              :retrieve true)))
+        properties {:protocol "free"
+                    :host "localhost"
+                    :port 4334
+                    :data-dir (file
+                               (System/getProperty "user.dir")
+                               "target" "datomic-data")
+                    :log-dir (file
+                              (System/getProperty "user.dir")
+                              "target" "datomic-logs")}]
+    (.deleteOnExit f)
+    (save-properties f properties)
+    (eval-in cl `(require 'datomic.launcher))
+    (let [f (future
+              (eval-in cl `(do
+                             (datomic.launcher/-main ~(.getAbsolutePath f))
+                             (Thread/sleep 20000))))
+          out *out*
+          t (Thread. #(deref f))]
+      (.start t))
+    (loop [tries 6]
+      (if (pos? tries)
+        (when (port-reachable? "localhost" 4334)
+          (recur (dec tries)))
+        (throw (Exception. "Datomic failed to start in time"))))
+    (Thread/sleep 10000)))
+
+(defn populate-codeq
+  []
+  (let [cl (classlojure
+            (dependency-files
+             (resolve-dependencies
+              :repositories {"clojars" "https://clojars.org/repo/"
+                             "central" "http://repo1.maven.org/maven2/"}
+              :coordinates '[[org.cloudhoist/codeq "0.1.0-SNAPSHOT"
+                              :exclusions [org.slf4j/slf4j-nop]]
+                             [org.clojure/clojure "1.5.0-alpha6"]
+                             [ch.qos.logback/logback-classic "1.0.0"]]
+              :retrieve true)))]
+    (eval-in cl `(require 'datomic.codeq.core))
+    (eval-in cl `(import 'org.h2.Driver))  ;; fails to find driver without this
+    (eval-in cl `(datomic.codeq.core/main ~(datomic-url)))))
+
+(deftest codeq-def-test
+  (try
+    (run-transactor)
+    (populate-codeq)
+    (let [[in out transport] (queue-transport)
+          handler (-> unknown-op wrap-codeq-def)
+          [handle-f a] (stoppable-handle handler transport)
+          handle-loop (future
+                        (try
+                          (handle-f)
+                          (catch Exception e (print-cause-trace e))))]
+      ;; set the classpath
+      (.offer in {:op "codeq-def" :symbol "ritz.logging/trace"
+                  :datomic-url (datomic-url) :id 0})
+      (is (= {:value
+              [["(defmacro trace\n  [fmt-str & args]\n  `(log :trace ~fmt-str ~@args))"
+                "Sat Jul 02 22:08:35 EDT 2011"]]
+              :id 0}
+             (.take out)))
+
+      ;; shut down the handle loop
+      (reset! a false)
+      (.offer in {:op "something to stop the handle loop"}))))
