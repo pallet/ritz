78ec8aa81192c35ec26ff00b84013067b5d31518
Hugo Duncan <hugo@hugoduncan.org>
2012-11-17 14:13:42 -0500

    Add codeq middleware to dev profile

--- nrepl-codeq/profiles.clj
+++ nrepl-codeq/profiles.clj
@@ -1,7 +1,10 @@
 {:dev {:dependencies [[com.cemerick/pomegranate "0.0.13"
                        :exclusions [org.slf4j/slf4j-api org.clojure/clojure]]
                       [classlojure "0.6.6"
-                       :exclusions [org.slf4j/slf4j-api org.clojure/clojure]]]}
+                       :exclusions [org.slf4j/slf4j-api org.clojure/clojure]]]
+       :repl-options
+       {:nrepl-middleware
+        [ritz.nrepl.middleware.codeq/wrap-codeq-def]}}}
  :codox {:codox {:writer codox-md.writer/write-docs
                  :output-dir "../doc/0.5/nrepl-codeq/api"}
          :dependencies [[codox-md "0.1.0"]]
