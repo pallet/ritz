129560b678447447fc30c7015008496055a3149d
Hugo Duncan <hugo@hugoduncan.org>
2013-03-18 10:01:39 -0700

    Add missing dependency in project.clj

--- lein-ritz/project.clj
+++ lein-ritz/project.clj
@@ -1,4 +1,7 @@
 (defproject lein-ritz "0.7.1-SNAPSHOT"
   :description "A Leiningen plugin for launching a ritz swank server for SLIME."
-  :dependencies [[org.clojure/tools.cli "0.2.2"]]
+  :dependencies [[org.clojure/tools.cli "0.2.2"
+                  :exclusions [org.clojure/clojure]]
+                 [org.clojure/tools.nrepl "0.2.1"
+                  :exclusions [org.clojure/clojure]]]
   :eval-in-leiningen true)
