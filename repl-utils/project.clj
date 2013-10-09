c4b0249f1111903cc646f30edea474098b7ec2e8
Charles Comstock <dgtized@gmail.com>
2013-10-09 15:33:08 -0500

    Actually debugger is the subproject requiring 1.4.0

--- repl-utils/project.clj
+++ repl-utils/project.clj
@@ -4,5 +4,5 @@
   :scm {:url "git@github.com:pallet/ritz.git"}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.tcrawley/dynapath "0.2.3"]])
